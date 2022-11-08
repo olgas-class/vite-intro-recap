2b5833ab4b3e674fece1ff51fdca8edcba8228a3
olga-demina <olga.demina1410@gmail.com>
2022-11-08 11:13:36 +0100

    Esempi di componenti

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppBanner from "./components/AppBanner.vue";
+
+export default {
+  data() {
+    return {
+      title: "Mio titolo"
+    }
+  },
+  components: {
+    AppBanner
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section>
+    <h1>{{ title }}</h1>
+  </section>
+  <AppBanner />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style>
+h1 {
+  color: blue;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+h2 {
+  color: brown;
 }
 </style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: "AppBanner",
+    data() {
+        return {
+            title: "Banner title",
+            imgName: "fondo-pag-speciali.jpg",
+            // secondImg: "..."
+        }
+    },
+    methods: {
+        getImagePath(imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <section>
+        <h2>Titolo del banner</h2>
+        <img :src="getImagePath(`../assets/img/${this.imgName}`)" alt="">
+        <!-- <img :src="getImagePath(`../assets/img/${this.secondImg}`)" alt=""> -->
+        <!-- <img src="/img/fondo-pag-speciali.jpg" alt=""> -->
+    </section>
+</template>
+
+<style scoped>
+img {
+    max-width: 100%;
+}
+
+h2 {
+    color: green;
+}
+</style>
